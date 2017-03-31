c2a25db0d5d1b9124d5a98fe3263b1796d6dcf33
tblanchard <tblanchard@mac.com>
2017-03-31 09:13:03 -0700

    Initial Commit

---
+++ ObjectiveFMDB/ObjectiveFMDB.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveFMDB.h
+//  ObjectiveFMDB
+//
+//  Created by Todd Blanchard on 3/31/17.
+//  Copyright © 2017 Todd Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectiveFMDB.
+FOUNDATION_EXPORT double ObjectiveFMDBVersionNumber;
+
+//! Project version string for ObjectiveFMDB.
+FOUNDATION_EXPORT const unsigned char ObjectiveFMDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveFMDB/PublicHeader.h>
+
+
